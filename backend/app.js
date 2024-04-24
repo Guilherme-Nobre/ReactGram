c8ca47533e8584134e645569904f5d0c6934186c
Guilherme-Nobre <guilherme0906@gmail.com>
2024-04-23 23:16:16 -0300

    up

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,30 @@
+require("dotenv").config()
+
 const express = require("express"); // O require está carregando o modulo express do Node.js
 const path = require("path"); // O require está carregando o modulo path do Node.js
 const cors = require("cors"); // O require está carregando o modulo cors do Node.js
 
-const port = 5000;
+const port = process.env.PORT;
 
 const app = express();
 
 // config JSON and form data response
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Solve CORS
+app.use(cors({credentials: true, origin: "http://localhost:3000"}));
+
+// Upload directory
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+// DB Connection
+require("./config/db.js")
+
+// routes
+const router = require("./routes/Router.js");
+
+app.use(router);
 
 app.listen(port, () => {
     console.log(`App rodando da porta ${port}`);
