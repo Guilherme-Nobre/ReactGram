3e33d070981f7c321998a64d88dc5945c531ae5f
Guilherme-Nobre <guilherme0906@gmail.com>
2024-05-06 22:50:46 -0300

    ip

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,27 +1,28 @@
 const User = require("../models/User");
 
 const bcrypt = require("bcryptjs");
+const { profile } = require("console");
 const jwt = require("jsonwebtoken");
 
 const jwtSecret = process.env.JWT_SECRET;
 
 // Generate user token
 const generateToken = (id) => {
-    return jwt.sign({id}, jwtSecret, {
+    return jwt.sign({ id }, jwtSecret, {
         expiresIn: "7d",
     })
 };
 
 // Register user and sign in
-const register = async(req,res) =>{
-    
-    const {name, email, password} = req.body
+const register = async (req, res) => {
+
+    const { name, email, password } = req.body
 
     // check if user exists
-    const user = await User.findOne({email})
+    const user = await User.findOne({ email })
 
-    if (user){
-        res.status(422).json({errors: ["Por Favor, utilize outro e-mail."]})
+    if (user) {
+        res.status(422).json({ errors: ["Por Favor, utilize outro e-mail."] })
         return
     }
 
@@ -31,14 +32,14 @@ const register = async(req,res) =>{
 
     // create user
     const newUser = await User.create({
-        name, 
+        name,
         email,
         password: passwordHash
     })
 
     // if user was created successfully, return the token
-    if(!newUser){
-        res.status(422).json({errors: ["Houve um erro, por favor tente mais tarde."]})
+    if (!newUser) {
+        res.status(422).json({ errors: ["Houve um erro, por favor tente mais tarde."] })
         return
     }
 
@@ -50,11 +51,43 @@ const register = async(req,res) =>{
 };
 
 // sign user in
-const login = (req, res)=>{
-    res.send("Login")
-}
+const login = async (req, res) => {
+
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email })
+
+    //Check if user exists
+    if (!user) {
+        res.status(404).json({ errors: ["Usuário não encontrado."] })
+        return
+    }
+
+    //Check if password matches
+    if (!(await bcrypt.compare(password, user.password))) {
+        res.status(422).json({ errors: ["Senha inválida."] })
+        return
+    }
+
+    //return user with token
+    res.status(201).json({
+        _id: user._id,
+        profileImage: user.profileImage,
+        token: generateToken(user._id),
+    });
+
+};
+
+
+// Get current logged in user
+const getCurrentUser = async (req, res) => {
+    const user = req.user;
+
+    res.status(200).json(user);
+};
 
 module.exports = {
     register,
-    login
+    login,
+    getCurrentUser,
 };
