3d134a8d21bb67cf248ac0eb39a1b53f4354fd6a
Guilherme-Nobre <guilherme0906@gmail.com>
2024-05-06 21:51:56 -0300

    up

--- backend/routes/Router.js
+++ backend/routes/Router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express();
 
-router.use("/api/users", require("./UserRoutrer"))
+router.use("/api/users", require("./UserRoutes"))
 
 // teste route
 
